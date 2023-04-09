e04beb06aea4f36db46f2c4d1a5622344ff28c79
Anushachepuri <130271450+Anushachepuri@users.noreply.github.com>
2023-04-09 13:52:58 +0530

    Update streamlit_app.py
    adding some new attributes to to the frame.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('BreakFast Menu')
+steamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothi')
+streamlit.text('Hard-Boiled Free-Range Egg')
